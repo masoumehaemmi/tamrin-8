5e0782842f4fe86049601f2505cbee2f818f1fce
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-08-23 21:31:18 +0430

    Add files via upload

---
+++ Calculate the fraction.py
@@ -0,0 +1,24 @@
+def sum_fraction(x,y):
+    result={}
+    result["s"]=x["s"] * y["m"] + x["m"] * y["s"]
+    result["m"]=x["m"] * y["m"]
+    return result
+def mul_fraction(x,y):
+    result={}
+    result["s"] = x ["s"] * y ["s"]
+    result["m"] = x["s"] * y["s"]
+    return result
+def sub_fraction(x,y):
+    result={}
+    result["s"]= x ["s"] * y ["m"] - x["m"] * y["s"]
+    result["m"]= x["m"] * y["m"]
+    return result
+def div_fraction(x,y):
+    result={}
+    result["s"]= x["s"] * y["m"]
+    result["m"]= x["m"] * y["s"]
+    return result
+
+a={"s": 4, "m": 6}
+b={"s":5 , "m": 8}
+print("sum :" , sum_fraction(a,b)  ,"mul:" ,mul_fraction(a,b),"sub:" , sub_fraction(a,b), "div:" ,div_fraction(a,b)  )
